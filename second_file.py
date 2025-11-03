9bf8b903f7276be43a564eef90e6a1544dc9943e
SaraSouhail <souhailsara31@gmail.com>
2025-11-03 18:47:05 +0100

    Update second_file.py and add third_file.py

---
+++ second_file.py
@@ -0,0 +1,3 @@
+# the white file first_file.py means it is added to github 
+# the red / orange file second_file.py means it is  been added to gihtub  but u modify it (like i do now when i write thse comments)
+# the green file third_file.py means it is not been added to gihtub repo yet 
